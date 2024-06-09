ee0ac614b741f5c9fef65de675c5eeed8a06bead
sergiosales <paulounopar@hotmail.com>
2024-06-09 14:37:50 -0300

    Atualizacao da Classe Avaliação de Produtos

--- src/main/java/com/sales/lojavirtual/model/AvaliacaoProduto.java
+++ src/main/java/com/sales/lojavirtual/model/AvaliacaoProduto.java
@@ -1,6 +1,7 @@
 package com.sales.lojavirtual.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
@@ -34,8 +35,75 @@ public class AvaliacaoProduto implements Serializable {
 	
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id",nullable = false,
-	foreignKey = @ForeignKey(value =ConstraintMode.CONSTRAINT,name = "pessoa_fk" ))
+	foreignKey = @ForeignKey(value =ConstraintMode.CONSTRAINT,name = "pessoas_fk" ))
 	private Pessoa pessoa;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getNota() {
+		return nota;
+	}
+
+	public void setNota(Integer nota) {
+		this.nota = nota;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AvaliacaoProduto other = (AvaliacaoProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "AvaliacaoProduto [id=" + id + ", nota=" + nota + ", descricao=" + descricao + ", produto=" + produto
+				+ ", pessoa=" + pessoa + "]";
+	}
+	
+	
+	
+	
 	
 
 }
